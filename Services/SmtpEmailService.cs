050c07617ce3f30d7c67d549fbfc2f54b12019d0
Your Name <you@email.com>
2026-03-17 18:03:25 +0530

    new_changes

--- Services/SmtpEmailService.cs
+++ Services/SmtpEmailService.cs
@@ -17,23 +17,31 @@ public class SmtpEmailService : IEmailService
         var smtpSection = _config.GetSection("EmailSettings");
 
         var client = new SmtpClient(smtpSection["SmtpHost"])
-        {
-            Port = int.Parse(smtpSection["SmtpPort"]!),
-            Credentials = new NetworkCredential(
-                smtpSection["SmtpUser"],
-                smtpSection["SmtpPassword"]),
-            EnableSsl = bool.Parse(smtpSection["EnableSsl"]!)
-        };
+{
+    Port = int.Parse(smtpSection["SmtpPort"]!),
+    Credentials = new NetworkCredential(
+        smtpSection["SmtpUser"],
+        smtpSection["SmtpPassword"]),
+    EnableSsl = bool.Parse(smtpSection["EnableSsl"]!),
 
-        var fromEmail = smtpSection["SmtpUser"] 
+    // 🔥 fixes
+    Timeout = 30000,
+    DeliveryMethod = SmtpDeliveryMethod.Network,
+    UseDefaultCredentials = false
+};
+
+var fromEmail = smtpSection["SmtpUser"] 
     ?? throw new Exception("SMTP user not configured.");
 
-    var mail = new MailMessage(
+var mail = new MailMessage(
     fromEmail,
     toEmail,
     subject,
-    body);
+    body
+);
 
-        await client.SendMailAsync(mail);
-    }
+mail.IsBodyHtml = true; // optional but recommended
+
+await client.SendMailAsync(mail);
+}
 }
