bd8b11ef2105a36fb38d0881e88c4fbfa32fc507
Your Name <you@email.com>
2026-03-17 21:29:12 +0530

    new_changes

--- Services/MailjetEmailService.cs
+++ Services/MailjetEmailService.cs
@@ -19,26 +19,29 @@ public class MailjetEmailService : IEmailService
 
         var client = new MailjetClient(apiKey, secretKey);
 
-        var request = new MailjetRequest
-        {
-            Resource = Send.Resource,
-        }
-        .Property(Send.FromEmail, _config["EmailSettings:FromEmail"])
-        .Property(Send.FromName, "LMS")
-        .Property(Send.Subject, subject)
-        .Property(Send.TextPart, body)
-        .Property(Send.HtmlPart, $"<h3>{body}</h3>")
-        .Property(Send.Recipients, new JArray {
-            new JObject {
-                {"Email", toEmail}
-            }
-        });
-
-        var response = await client.PostAsync(request);
-
-        if (!response.IsSuccessStatusCode)
-        {
-            throw new Exception("Mailjet error: " + response.GetErrorMessage());
-        }
+       var request = new MailjetRequest
+{
+    Resource = Send.Resource,
+}
+.Property(Send.FromEmail, _config["EmailSettings:FromEmail"])
+.Property(Send.FromName, "LMS")
+.Property(Send.Subject, subject)
+.Property(Send.TextPart, body)
+.Property(Send.HtmlPart, $"<h3>{body}</h3>")
+.Property(Send.Recipients, new JArray {
+    new JObject {
+        {"Email", toEmail}
     }
+});
+
+var response = await client.PostAsync(request);  // 🔥 yahi
+
+Console.WriteLine("Mailjet Status: " + response.StatusCode);
+
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine("Mailjet Error: " + response.GetErrorMessage());
+    throw new Exception("Mailjet error: " + response.GetErrorMessage());
+}
+}
 }
